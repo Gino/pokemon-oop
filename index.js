9e591bc1d4c7167f64100b1202e25a2dbba1ba13
Gino Louwsma <ginolouwsma01@gmail.com>
2019-03-13 13:50:40 +0100

    Added some logs that prints the health before any attacks, and after the attacks.

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const routes = require('./router.js')
 let Pikachu = require('./classes/pokemons/Pikachu.js')
 let Charmeleon = require('./classes/pokemons/Charmeleon.js')
 
-init = () => {
-  Pikachu = new Pikachu
-  Charmeleon = new Charmeleon
-}
-
 attack = (attacker, opponent, attack) => {
   attack = attacker.getAttack(attack)
   if (attack === null) return
@@ -19,9 +14,6 @@ attack = (attacker, opponent, attack) => {
 
   const opponentWeakness = opponent.effects[0]
   const opponentResistance = opponent.effects[1]
-
-  const attackerWeakness = attacker.effects[0]
-  const attackerResistance = attacker.effects[1]
 
   // If the attacker his energy type is equal to the weakness of the opponent, multiply the damage of the attack.
   if (attacker.energyType === opponentWeakness.energyType) {
@@ -35,10 +27,27 @@ attack = (attacker, opponent, attack) => {
 
   let healthAfterAttack = opponentHealth - damage
 
-  console.log(opponent.name + "'s health: " + healthAfterAttack)
+  console.log(attacker.name + " will now attack " + opponent.name + " with a " + attack.name + " attack")
+  
+  setTimeout(() => {
+    console.log(opponent.name + "'s health: " + healthAfterAttack)
+  }, 2000);
+}
+
+init = () => {
+  Pikachu = new Pikachu
+  Charmeleon = new Charmeleon
+
+  console.log(Pikachu.name + "'s health starts at: " + Pikachu.health)
+  console.log(Charmeleon.name + "'s health starts at: " + Charmeleon.health)
+
+  setTimeout(() => {
+    attack(Pikachu, Charmeleon, 'Electric Ring')
+  }, 5000)
+
+  setTimeout(() => {
+    attack(Charmeleon, Pikachu, 'Head Butt')
+  }, 10000)
 }
 
 init()
-
-attack(Pikachu, Charmeleon, 'Electric Ring')
-attack(Charmeleon, Pikachu, 'Head Butt')
